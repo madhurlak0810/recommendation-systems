a11d0f9d25ac7b0a5f3093d40d35694496fb49a6
Joshua Liu <jliu3714@gmail.com>
2025-04-07 01:01:43 -0400

    created script to download data from s3

---
+++ s3_data.py
@@ -0,0 +1,30 @@
+import boto3
+import os
+import pandas as pd
+import sys
+
+def download_data(s3, local_dir):
+    for obj in response["Contents"]:
+        s3.download_file(bucket_name, obj["Key"], os.path.join(local_dir, os.path.basename(obj["Key"])))
+
+if __name__ == "__main__":
+    # usage: python s3_data.py user_accessKeys.csv 
+    bucket_name = "your-recommender-data"
+    prefix = "archive (1)/"
+    local_dir = "data/"
+    os.makedirs(local_dir, exist_ok=True)
+
+    if len(sys.argv) > 1:
+        df_cred = pd.read_csv(sys.argv[1])
+        s3 = boto3.client(
+            "s3", 
+            aws_access_key_id=df_cred["Access key ID"].values[0], 
+            aws_secret_access_key=df_cred["Secret access key"].values[0], 
+            region_name="us-east-1")
+    else:
+        s3 = boto3.client("s3")
+
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    download_data(s3, local_dir)
+
