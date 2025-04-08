79ba78bcc23f08aec6037edbd85cddd464d7eab1
Joshua Liu <jliu3714@gmail.com>
2025-04-07 21:10:50 -0400

    fixing some csv

--- s3_data.py
+++ s3_data.py
@@ -1,10 +1,13 @@
 import boto3
 import os
 import pandas as pd
+import numpy as np
 import sys
+import tqdm
+import glob
 
 def download_data(s3, local_dir):
-    for obj in response["Contents"]:
+    for obj in tqdm.tqdm(response["Contents"]):
         s3.download_file(bucket_name, obj["Key"], os.path.join(local_dir, os.path.basename(obj["Key"])))
 
 if __name__ == "__main__":
@@ -14,6 +17,7 @@ if __name__ == "__main__":
     local_dir = "data/"
     os.makedirs(local_dir, exist_ok=True)
 
+    # connect to s3 instance
     if len(sys.argv) > 1:
         df_cred = pd.read_csv(sys.argv[1])
         s3 = boto3.client(
@@ -24,7 +28,7 @@ if __name__ == "__main__":
     else:
         s3 = boto3.client("s3")
 
+    # get data from s3 instance
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
 
     download_data(s3, local_dir)
-
