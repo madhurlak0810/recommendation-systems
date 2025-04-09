6cb17b06ac0fa559b25208e404089fe7dd596135
Joshua Liu <jliu3714@gmail.com>
2025-04-08 20:55:51 -0400

    import fixes

--- update_model/app/services/preprocess.py
+++ update_model/app/services/preprocess.py
@@ -3,15 +3,17 @@ import re
 
 class PreprocessService:
     @staticmethod
-    def clean_dataframe(df):
+    def clean_dataframe(df: pd.DataFrame):
+        if "Unnamed: 0" in df.columns:
+            df.drop("Unnamed: 0", axis=1, inplace=True)
+
         df["ratings"] = pd.to_numeric(df.get("ratings"), downcast="float", errors="coerce")
         df["no_of_ratings"] = pd.to_numeric(df.get("no_of_ratings"), downcast="float", errors="coerce")
+        
 
         for price_col in ["discount_price", "actual_price"]:
             if price_col in df.columns:
-                df[price_col] = df[price_col].astype(str).apply(
-                    lambda x: float(re.sub(r"[^\d.]", "", x)) if x.strip() else None
-                )
+                df[price_col] = df[price_col].astype(str).apply(lambda x: float(str(x).strip("₹").replace(",","")))
 
         return df
 
